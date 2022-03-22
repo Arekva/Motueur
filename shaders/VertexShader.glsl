8cd1398685238a866f2ccc3d7d29f04423a78d8b
SSSamot <tom.vendeville@orange.fr>
2022-03-22 16:38:21 +0100

    Create VertexShader.glsl
    oops

---
+++ shaders/VertexShader.glsl
@@ -0,0 +1,28 @@
+#version 330 core
+layout(location = 0) in vec3 vertexPosition_modelspace;
+layout(location = 1) in vec2 vertexUV;
+layout(location = 2) in vec3 normals;
+
+out vec2 UV;
+out vec3 normal;
+out vec3 light;
+out float lightPower;
+out vec3 pos;
+
+uniform mat4 MVP;
+uniform mat4 View;
+uniform mat4 Model;
+uniform vec3 LightWorld;
+
+void main(){
+    
+    gl_Position =  MVP * vec4(vertexPosition_modelspace,1);
+    vec3 Position_worldspace = (Model * vec4(vertexPosition_modelspace,1)).xyz;  
+    vec3 vertexPosition_cameraspace = ( View * Model * vec4(vertexPosition_modelspace,1)).xyz;
+        
+    vec3 lightPos = ( View * vec4(LightWorld.xyz,1)).xyz;
+    light = lightPos - vertexPosition_cameraspace;
+    UV = vertexUV;
+    normal = (View*Model*vec4(normals,0)).xyz;
+
+}
